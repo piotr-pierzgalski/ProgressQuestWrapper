76676c47f9217be361d3aae39298af465fb45392
Piotr <piotr.pierzgalski@epicode.net>
2017-06-01 13:10:08 +0200

    v 0.2

---
+++ ProgressQuestWrapper/WrapperForm.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace ProgressQuestWrapper
+{
+    partial class WrapperForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.wrapperPanel = new System.Windows.Forms.Panel();
+            this.autostartLabel = new System.Windows.Forms.Label();
+            this.autostartStatusCheckbox = new System.Windows.Forms.CheckBox();
+            this.syncStatusLabel = new System.Windows.Forms.Label();
+            this.syncButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // wrapperPanel
+            // 
+            this.wrapperPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.wrapperPanel.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.wrapperPanel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.wrapperPanel.Location = new System.Drawing.Point(12, 57);
+            this.wrapperPanel.Name = "wrapperPanel";
+            this.wrapperPanel.Size = new System.Drawing.Size(336, 224);
+            this.wrapperPanel.TabIndex = 0;
+            this.wrapperPanel.SizeChanged += new System.EventHandler(this.wrapperPanel_SizeChanged);
+            // 
+            // autostartLabel
+            // 
+            this.autostartLabel.AutoSize = true;
+            this.autostartLabel.Location = new System.Drawing.Point(13, 17);
+            this.autostartLabel.Name = "autostartLabel";
+            this.autostartLabel.Size = new System.Drawing.Size(49, 13);
+            this.autostartLabel.TabIndex = 1;
+            this.autostartLabel.Text = "Autostart";
+            // 
+            // autostartStatusCheckbox
+            // 
+            this.autostartStatusCheckbox.AutoSize = true;
+            this.autostartStatusCheckbox.Location = new System.Drawing.Point(68, 16);
+            this.autostartStatusCheckbox.Name = "autostartStatusCheckbox";
+            this.autostartStatusCheckbox.Size = new System.Drawing.Size(80, 17);
+            this.autostartStatusCheckbox.TabIndex = 2;
+            this.autostartStatusCheckbox.Text = "checkBox1";
+            this.autostartStatusCheckbox.UseVisualStyleBackColor = true;
+            this.autostartStatusCheckbox.CheckedChanged += new System.EventHandler(this.autostartStatusCheckbox_CheckedChanged);
+            // 
+            // syncStatusLabel
+            // 
+            this.syncStatusLabel.AutoSize = true;
+            this.syncStatusLabel.Location = new System.Drawing.Point(313, 17);
+            this.syncStatusLabel.Name = "syncStatusLabel";
+            this.syncStatusLabel.Size = new System.Drawing.Size(37, 13);
+            this.syncStatusLabel.TabIndex = 3;
+            this.syncStatusLabel.Text = "Status";
+            // 
+            // syncButton
+            // 
+            this.syncButton.Location = new System.Drawing.Point(232, 12);
+            this.syncButton.Name = "syncButton";
+            this.syncButton.Size = new System.Drawing.Size(75, 23);
+            this.syncButton.TabIndex = 4;
+            this.syncButton.Text = "Synchronize";
+            this.syncButton.UseVisualStyleBackColor = true;
+            this.syncButton.Click += new System.EventHandler(this.syncButton_Click);
+            // 
+            // WrapperForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(360, 293);
+            this.Controls.Add(this.syncButton);
+            this.Controls.Add(this.syncStatusLabel);
+            this.Controls.Add(this.autostartStatusCheckbox);
+            this.Controls.Add(this.autostartLabel);
+            this.Controls.Add(this.wrapperPanel);
+            this.Name = "WrapperForm";
+            this.Text = "ProgressQuest Wrapper";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.WrapperForm_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel wrapperPanel;
+        private System.Windows.Forms.Label autostartLabel;
+        private System.Windows.Forms.CheckBox autostartStatusCheckbox;
+        private System.Windows.Forms.Label syncStatusLabel;
+        private System.Windows.Forms.Button syncButton;
+    }
+}
+
---
+++ ProgressQuestWrapper/WrapperForm.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace ProgressQuestWrapper
+{
+    public partial class WrapperForm : Form
+    {
+        RegistryKey rkApp = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+        private IntPtr progressQuestWindowHandle;
+        public WrapperForm()
+        {
+            InitializeComponent();
+
+            Process p = new Process();
+            p.StartInfo = new ProcessStartInfo() { FileName = "ProgressQuest\\Fithvael_2.0 [Spoltog].pq" };
+            p.Start();
+            p.WaitForInputIdle(); // Allow the process to open it's window
+            progressQuestWindowHandle = FindWindowByCaption(IntPtr.Zero, "ProgressQuest - Fithvael_2.0 [Spoltog]");
+
+
+            RECT rct;
+            if (!GetWindowRect(new HandleRef(wrapperPanel, progressQuestWindowHandle), out rct))//p.MainWindowHandle
+            {
+                MessageBox.Show("ERROR");
+                return;
+            }
+
+            //wrapperPanel.Dock = DockStyle.Fill;
+
+            this.Height = 700;//rct.Bottom - rct.Top + 100;
+            this.Width = 700;//rct.Right - rct.Left + 40;
+            //wrapperPanel.Height = 660;
+
+            SetParent(progressQuestWindowHandle, wrapperPanel.Handle);
+            //MoveWindow(progressQuestWindowHandle, this.Left + 11, this.Top + 40, this.Width - 40, this.Height - 80, true);
+            MoveWindow(progressQuestWindowHandle, 1, 1, wrapperPanel.Width-7, wrapperPanel.Height-7, true);
+
+            if (rkApp != null && rkApp.GetValue("ProgressQuestWrapper") == null)
+            {
+                autostartStatusCheckbox.Checked = false;
+            }
+            else
+            {
+                autostartStatusCheckbox.Checked = true;
+            }
+        }
+
+        [DllImport("user32.dll", EntryPoint = "FindWindow")]
+        public static extern IntPtr FindWindowByCaption(IntPtr ZeroOnly, string lpWindowName);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool GetWindowRect(HandleRef hWnd, out RECT lpRect);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern IntPtr SendMessage(IntPtr hWnd, UInt32 Msg, IntPtr wParam, IntPtr lParam);
+
+        private const UInt32 WM_CLOSE = 0x0010;
+
+        [DllImport("user32.dll", SetLastError = true)]
+        internal static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int nWidth, int nHeight, bool bRepaint);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern UInt32 GetWindowLong(IntPtr hWnd, int nIndex);
+
+        [DllImport("user32.dll")]
+        static extern int SetWindowLong(IntPtr hWnd, int nIndex, UInt32 dwNewLong);
+
+        void CloseWindow(IntPtr hwnd)
+        {
+            SendMessage(hwnd, WM_CLOSE, IntPtr.Zero, IntPtr.Zero);
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
+        {
+            public int Left;        // x position of upper-left corner
+            public int Top;         // y position of upper-left corner
+            public int Right;       // x position of lower-right corner
+            public int Bottom;      // y position of lower-right corner
+        }
+
+        private void autostartStatusCheckbox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (autostartStatusCheckbox.Checked)
+            {
+                rkApp.SetValue("ProgressQuestWrapper", Application.ExecutablePath);
+            }
+            else
+            {
+                rkApp.DeleteValue("ProgressQuestWrapper", false);
+            }
+        }
+
+        private void WrapperForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            CloseWindow(progressQuestWindowHandle);
+        }
+
+        private void syncButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void wrapperPanel_SizeChanged(object sender, EventArgs e)
+        {
+            MoveWindow(progressQuestWindowHandle, 1, 1, wrapperPanel.Width - 7, wrapperPanel.Height - 7, true);
+        }
+    }
+}
